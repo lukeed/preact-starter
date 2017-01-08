1ff8a906bf81305226c440049e49da7adddeff3b
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 12:37:06 -0800

    print url within 404

--- src/views/pages/errors/404.js
+++ src/views/pages/errors/404.js
@@ -1,12 +1,13 @@
 import { h } from 'preact';
 import Card from '../../tags/card';
 
-export default function () {
+export default function (props) {
 	return (
 		<div className="page page__404">
 			<Card>
 				<h1>404 Page</h1>
 				<p>Looks like you were given a bad link ;-)</p>
+				<pre>{ props.url }</pre>
 			</Card>
 		</div>
 	);
