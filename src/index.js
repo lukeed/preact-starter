a90ef61b35bdc06a5db3df0a4d3a4da5ef683596
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 13:12:33 -0800

    add tracking id

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-XXXXXXXX-X', 'auto');
+	ga('create', 'UA-71341501-3', 'auto');
 	ga('send', 'pageview');
 } else {
 	require('preact/devtools');
