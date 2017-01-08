af63438e7b0d45974ef340e87f490565c5ef3ada
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 13:09:15 -0800

    add dev tools to homepage

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -25,6 +25,7 @@ export default function (props) {
 					<li>ES2015 (ES6) and ES2016 (ES7) support</li>
 					<li>Webpack Bundle Analysis (via `webpack-dashboard`)</li>
 					<li>Hot Module Replacement (HMR) for all files</li>
+					<li>Preact Developer Tools</li>
 					<li><a href="https://github.com/GoogleChrome/lighthouse" target="_blank" rel="noopener">Lighthouse</a> approved (100/100)</li>
 				</ul>
 			</Card>
