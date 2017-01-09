8fb763199820d0dca797bd01f12569f2689603dd
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 17:41:41 -0800

    derp; :man_facepalming: (#1)

--- src/views/index.js
+++ src/views/index.js
@@ -9,9 +9,7 @@ import Credit from './pages/credit';
 import Blog from './pages/blog';
 
 // track pages on route change
-const onChange = props => {
-	window.ga && history.listen(obj => ga('send', 'pageview', obj.url));
-};
+const onChange = obj => window.ga && ga('send', 'pageview', obj.url);
 
 export default (
 	<Layout>
