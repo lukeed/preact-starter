e77b8e3a19068d3cceb8a8b680910b8d7f945965
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 17:33:08 -0800

    rename cacheID to preact-starter

--- config/setup.js
+++ config/setup.js
@@ -40,7 +40,7 @@ if (isProd) {
 		new webpack.optimize.UglifyJsPlugin(uglify),
 		new ExtractText('styles.[hash].css'),
 		new SWPrecache({
-			cacheId: 'inferno-starter',
+			cacheId: 'preact-starter',
 			filename: 'service-worker.js',
 			dontCacheBustUrlsMatching: /./,
 			navigateFallback: 'index.html',
